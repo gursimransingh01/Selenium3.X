1efc60cf28fd1b5f7c665a99372b8db2a5ec7740
gursimransingh01 <gursimransingh@Gursimrans-MacBook-Pro.local>
2017-01-02 16:46:32 -0500

    Opening google.com on firefox

--- src/simpleTestCasesOnBrowsers/FirefoxTest.java
+++ src/simpleTestCasesOnBrowsers/FirefoxTest.java
@@ -1,8 +1,17 @@
 package simpleTestCasesOnBrowsers;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class FirefoxTest {
-public static void main(String[] args) {
-	
-}
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "/Users/gursimransingh/Documents/workspace/geckodriver");
+		driver = new FirefoxDriver();
+		String baseURL = "http://www.google.com";
+		
+		driver.get(baseURL);
+	}
 
 }
