4fcad2838609096f5f2f3f31dead069d5af06297
gursimransingh01 <gursimransingh@Gursimrans-MacBook-Pro.local>
2017-01-02 17:45:32 -0500

    Opening google using Chrome

---
+++ src/simpleTestCasesOnBrowsers/ChromeTest.java
@@ -0,0 +1,20 @@
+package simpleTestCasesOnBrowsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeTest {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "/Users/gursimransingh/Downloads/chromedriver");
+		
+		driver = new ChromeDriver();
+		
+		String baseURL = "http://www.google.com";
+		
+		driver.get(baseURL);
+		
+	}
+
+}
