e52a2c4cfe84e69bc24b2707dfa8f22e924a5154
gursimransingh01 <gursimransingh@Gursimrans-MacBook-Pro.local>
2017-01-03 23:13:30 -0500

    Locating element (id and xpath)

---
+++ src/locationElement/IdXpathDemo.java
@@ -0,0 +1,27 @@
+package locationElement;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class IdXpathDemo {
+
+	public static void main(String[] args) {
+		// XPath .//*[@id='tsf']/div[2]/div[3]/center/input[1]
+		// Id gs_htif0
+		
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "/Users/gursimransingh/Documents/workspace/geckodriver");
+		driver = new FirefoxDriver();
+		String baseURL = "http://www.google.com";
+		
+		driver.get(baseURL);
+		
+		driver.findElement(By.id("gs_htif0")).sendKeys("facebook");
+		driver.findElement(By.xpath(".//*[@id='_fZl']")).click();
+		
+		
+
+	}
+
+}
